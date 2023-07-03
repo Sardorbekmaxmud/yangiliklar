9d9c7d037975ab1f94efa2aa2e1e0690b821bd47
Sardorbekmaxmud <sardorbekmaxmudov33@gmail.com>
2023-07-03 10:13:50 +0500

    news project commit

---
+++ yangiliklar/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import (AllCategoryApiView,DetailCategoryApiView,CreateCategoryApiView,UpdateCategoryApiView,DeleteCategoryApiView,
+                    AllNewsApiView,DetailNewApiView,CreateNewApiView,UpdateNewApiView,DeleteNewApiView)
+
+urlpatterns = [
+    #category------------------------------------------------------------------------
+    path('category/',AllCategoryApiView.as_view()),
+    path('category/<int:category_id>/',DetailCategoryApiView.as_view()),
+    path('category/create/',CreateCategoryApiView.as_view()),
+    path('category/update/<int:category_id>/',UpdateCategoryApiView.as_view()),
+    path('category/delete/<int:category_id>/',DeleteCategoryApiView.as_view()),
+    # news ---------------------------------------------------------------------------
+    path('news/',AllNewsApiView.as_view()),
+    path('news/<int:new_id>/',DetailNewApiView.as_view()),
+    path('news/create/',CreateNewApiView.as_view()),
+    path('news/update/<int:new_id>/',UpdateNewApiView.as_view()),
+    path('news/delete/<int:new_id>/',DeleteNewApiView.as_view()),
+]
